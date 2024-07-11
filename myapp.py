fabdd11e70affff2f932003d3c5841c11441b36f
JCieF <166143233+JCieF@users.noreply.github.com>
2024-07-12 04:23:35 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import load_model
 import numpy as np
 
 # Load the model
-model = load_model('model.h5')
+model = load_model('finalExam.h5')
 
 # Function to preprocess the URL (assuming your model requires some preprocessing)
 def preprocess_url(url):
